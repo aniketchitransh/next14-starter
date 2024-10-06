c17cdcd0145dee7c626474b9303abae352b9cab3
aniketchitransh <ani.chitransh@gmail.com>
2024-10-06 12:31:05 +0530

    Adding more links to the navbar. Currently stuck in an issue with React conflicting with Next Js, I think I need to install Typescript. Let's see.

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,31 +1,59 @@
-import Link from "next/link";
+"use client"
+
+import { useState } from 'react';
+import styles from './links.module.css';
+import NavLink from "./navLink/navLink";
+
+const links = [
+    {
+        title: "Home",
+        path: "/",
+    },
+    {
+        title: "About",
+        path: "/about",
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+    },
+    {
+        title: "Blog",
+        path: "/blog",
+    },
+];
 
 const Links = () => {
+    const [open, setOpen] = useState(false);
 
-    const links = [
-        {
-            title: "Home",
-            path: "/",
-        },
-        {
-            title: "About",
-            path: "/about",
-        },
-        {
-            title: "Contact",
-            path: "/contact",
-        },
-        {
-            title: "Blog",
-            path: "/blog",
-        },
-    ];
+    // Temporary
+    const session = true;
+    const isAdmin = true;
 
     return (
-        <div>
-            {links.map((link) => (
-                <Link href={link.path} key={link.title}>{link.title}</Link>
-            ))}
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link) => (
+                    <NavLink item={link} key={links.title} />
+                ))}{
+                    session ? (
+                        <>
+                            {isAdmin && (<NavLink item={{ title: "Admin", path: "/admin" }} />)}
+                            <button className={styles.logout}>Logout</button>
+                        </>
+                    ) : (
+                        <NavLink item={{ title: "Login", path: "/login" }} />
+                    )
+                }
+            </div>
+            <button>Menu</button>
+            {
+                open && <div className={styles.mobileLinks}>
+                    {links.map((link) => (
+                        <NavLink item={link} key={link.title} />
+                    ))}
+                </div>
+            }
         </div>
     )
 }
